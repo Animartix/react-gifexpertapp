98c531b2986c5ad064d11bff38b806d34c129b3b
Animartix <animartics@gmail.com>
2022-01-17 15:45:32 -0300

    First Commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+export const AddCategory = ( { setCategories }) => {
+
+    const [inputValue, setinputValue] = useState('');
+
+    const handleInputChange = ( e ) => {
+        setinputValue( e.target.value );
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        //console.log('Submit hecho')
+        setCategories( cats => [ inputValue, ...cats ]);
+    }
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <input
+                 type="text"
+                 value={ inputValue }
+                 onChange={ handleInputChange }
+            />
+        </form>
+    )
+}
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
